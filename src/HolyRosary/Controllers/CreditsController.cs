dce4337e193ed759887121a6c444a7bbd43255e4
Javier Lozano <javier@lozanotek.com>
2020-11-14 23:07:28 -0600

    Added credits for the resources used

--- src/HolyRosary/Controllers/CreditsController.cs
+++ src/HolyRosary/Controllers/CreditsController.cs
@@ -5,6 +5,9 @@ namespace HolyRosary.Controllers
     [Route("credits")]
     public class CreditsController : Controller 
     {
-
+        public IActionResult Index() 
+        {
+            return View();
+        }
     }
 }
